c1fcedd97e6dc4211e28e9051eaeed80c8e2a282
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-11 16:47:17 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -1,3 +1,4 @@
+Basic Queries exercise:
 SELECT name FROM students;
 SELECT * FROM students WHERE Age > 30;
 SELECT name FROM students WHERE Gender = "F" AND Age > 30;
@@ -6,6 +7,9 @@ INSERT INTO students (id, name, Age, Gender, Points) VALUES (7, "Emile", 23, "M"
 UPDATE students SET Points = "400" WHERE name = "Basma";
 UPDATE students SET Points = 100 WHERE name = "Alex";
 
+
+
+Creating Table exercise:
 CREATE TABLE graduates (
 	ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	Name TEXT NOT NULL UNIQUE,
@@ -17,3 +21,10 @@ CREATE TABLE graduates (
 INSERT INTO graduates(ID, name, Age, Gender, Points) SELECT ID, name, Age, Gender, Points FROM students WHERE name = "Layal";
 UPDATE graduates SET Graduation = "2018-09-08" WHERE name = "Layal";
 DELETE FROM students WHERE name = "Layal";
+
+
+
+Joins exercise:
+SELECT employees.Name, employees.Company, companies.Date FROM employees JOIN companies ON employees.Company = companies.name;
+SELECT employees.Name FROM employees JOIN companies ON employees.Company = companies.name WHERE companies.Date < 2000;
+SELECT companies.Name FROM companies JOIN employees ON companies.name = employees.Company WHERE employees.Role = "Graphic Designer";
