904df2d5f809be19452bcea5b3a402191317fd7f
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-11 16:47:46 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -28,3 +28,5 @@ Joins exercise:
 SELECT employees.Name, employees.Company, companies.Date FROM employees JOIN companies ON employees.Company = companies.name;
 SELECT employees.Name FROM employees JOIN companies ON employees.Company = companies.name WHERE companies.Date < 2000;
 SELECT companies.Name FROM companies JOIN employees ON companies.name = employees.Company WHERE employees.Role = "Graphic Designer";
+
+
